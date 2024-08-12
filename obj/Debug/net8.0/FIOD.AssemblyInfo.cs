8b411c6daffb8f1e69be5ab7763cba8edf805420
infolii <invateb@bk.ru>
2024-08-12 21:50:53 +0300

    Обнивил gitignor

--- obj/Debug/net8.0/FIOD.AssemblyInfo.cs
+++ obj/Debug/net8.0/FIOD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FIOD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be2eb306aff4371739344971e445cd50d87c082")]
 [assembly: System.Reflection.AssemblyProductAttribute("FIOD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FIOD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
