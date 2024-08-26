647efdab6ed0c222de3542cd0da3badb08dd2021
infolii <invateb@bk.ru>
2024-08-26 17:08:43 +0300

    ГОТОВАЯ ВЕРСИЯ (БЕЗ БД)
    //БД создастся сама
    
    //Некоторые ответы по ошибкам не будут работать при отсутвии аккаунтов в базе данных
    
    //Требуется тестирование, я протестировал как смог, но мог что то упустить

--- obj/Debug/net8.0/FIOD.AssemblyInfo.cs
+++ obj/Debug/net8.0/FIOD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FIOD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+309b5c9763362dc2902dfc1663a41069a18ba535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05fc65c190e1ba6ff87bcd875e9dc84e7531d04f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FIOD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FIOD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
